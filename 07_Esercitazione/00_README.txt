900f6a7f92b252a4e244ad6bf2e20c852e2b8edf
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-20 17:28:39 +0200

    risolto esercizio 2

--- 07_Esercitazione/00_README.txt
+++ 07_Esercitazione/00_README.txt
@@ -1,6 +1,6 @@
 Esercizi 19/10/2020
 
-1	Calcola la somma dei primi n. quadrat n quadrati 1^n, 2^n ecc
-2	Stamnpa le potenze di due minori o uguali a n
+1	Calcola la somma dei primi n. quadrat n quadrati 1^n, 2^n ecc OK
+2	Stamnpa le potenze di due minori o uguali a n 
 3	stampa le prime n potenze di 2
 4	Stampa un conto alla rovescia partendo da un numero n
---
+++ 07_Esercitazione/02_potenzeDue1/main.go
@@ -0,0 +1,27 @@
+// Stampare le potenze di due minori o uguali a n  
+
+package main
+
+import "fmt"
+
+func main() {
+	var n, p, p2 int
+
+	fmt.Print("Inserisci un numero ti dirò le potenze di 2 minori o uguali: ")
+	fmt.Scan(&n)
+	p = 1
+	p2 = 2
+
+	if n != 1 {
+		for p2 <= n {
+			if p <= 1{
+			fmt.Println("1")
+			}
+			p *= 2
+			p2 *= 2 
+			fmt.Println(p)
+		}
+	} else {
+		fmt.Println("1")
+	}	
+}
