caf16785b9242398106a7992dba90b50ec204a41
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-20 23:21:57 +0200

    risolto esercizio 4

---
+++ 07_Esercitazione/04_contoAllaRovescia/main.go
@@ -0,0 +1,19 @@
+// Stampa un conto alla rovescia partendo da un numero n
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Inserisci il numero di partenza per il conto alla rovescia: ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; {
+		fmt.Println(n)
+		n--
+		if n < 1 {
+			fmt.Println("Buon anno!")
+		}
+	}
+}
