10026a4da918d8a4b1b1dfb387b51a79e633b6c4
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-27 00:18:26 +0100

    risolto esercizio 1

---
+++ 10_Esercitazione/00_README.txt
@@ -0,0 +1,10 @@
+1 Sistemare il programma dell'epoca in modo che restituisca valori negativi se prima del 1970. 
+2 Scrivere un programma che dato n calcola il numero di numeri primi minori o uguali a n. Usarlo per verificare sperimentalmente che questo P(n) è asintotico per log di p(n) n/logn
+3 Dato n stapare i primi n numeri primi
+4 Disegnare il triangolo bucato 				
+
+*****
+*  *
+* *
+*
+									
---
+++ 10_Esercitazione/01_programmaEpoca/main.go
@@ -0,0 +1,49 @@
+// Sistemare il programma dell'epoca in modo che restituisca valori negativi se prima del 1970.  
+
+package main
+
+
+import "fmt"
+
+func main() {
+	var g, m, a int
+	fmt.Print("Inserisci una data: ")
+	fmt.Scan(&g)
+	fmt.Scan(&m)
+	fmt.Scan(&a)
+
+	fmt.Print(g, "/", m, "/", a)
+	fmt.Println()
+
+	gg := 0 
+	for anno := 1970; anno < a ; anno++ {
+		gg += 365
+		if anno % 4 == 0{
+			gg++
+		}
+	}
+
+	for anno := 1970; anno > a; anno-- {
+		gg -= 365
+		if anno % 4 == 0{
+			gg--
+		}
+	}
+	
+	for mese := 1; mese < m; mese++ {
+		if mese == 11 || mese == 4 || mese == 6 || mese == 9 {
+			gg += 30
+		} else if mese == 2 {
+			gg += 28
+			if a % 4 == 0 {
+				gg++
+			}
+		} else {
+			gg += 31
+		}
+	}
+
+	gg += g
+
+	fmt.Print("I giorni trascorsi dal 1/1/1970 al ", g, "/", m, "/", a, " sono passati ", gg, "\n")
+}
