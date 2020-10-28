5971b75335a6d281254d4178f03ed7153fd5f242
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-28 01:12:58 +0100

    risolto esercizio 4

---
+++ 10_Esercitazione/04_triangoloBucato/main.go
@@ -0,0 +1,42 @@
+/* Disegnare il triangolo bucato 
+
+*****
+*  *
+* *
+*
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int 
+	fmt.Print("Inserisci un numero: ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ { 			// for delle righe 
+
+		if i == n - 1 {
+			fmt.Println("*")
+			break
+		}
+		
+		if i == 0 {
+			for in := 0; in < n; in++ {
+				fmt.Print("*")
+			}
+		} else {
+				for {
+					fmt.Print("*")
+						for x := 0; x < (n - i) - 2; x++ {
+							fmt.Print(" ")
+						}
+					fmt.Print("*")
+					break	
+				}
+			}
+		fmt.Println()	
+	}
+}
