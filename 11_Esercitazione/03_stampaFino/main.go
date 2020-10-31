f45cd54a68275f8a67aa19edd84060f4fdfbc3dc
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-31 12:03:20 +0100

    iniziato esercizio 3, da completare

---
+++ 11_Esercitazione/03_stampaFino/main.go
@@ -0,0 +1,22 @@
+// Data una stringa ASCII, stampare tutto ciò che compare prima della prima '/'
+
+package main
+
+import (
+		"fmt"
+		"bufio"
+		"os"
+)
+
+func main() {
+	fmt.Print("Inserisci una stringa: ")
+	
+	b := bufio.NewScanner(os.Stdin)
+	b.Scan()
+	s := b.Text()
+
+	for _, x := range s {
+		fmt.Print(string(x))
+	}
+	fmt.Println()
+}
