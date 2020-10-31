ea322e094afc3b1fd1f854ba9ecc45fc18072679
viac92 <64146594+viac92@users.noreply.github.com>
2020-10-31 11:08:56 +0100

    risolto esercizio 1

---
+++ 11_Esercitazione/00_README.txt
@@ -0,0 +1,13 @@
+Esercizi:
+
+	1 Leggere una stringa e stampare quante "a" contiene OK
+		. Caso ASCII
+		. Caso Generale
+	
+	2 Come programma 1 ma ora il programma chiede di inserire continuamente una nuova linea 
+	  dopo ogni riga stamoare quante "a" conteneva.
+	  	. Alla fine stampare la media di tutte le "a" per riga.
+	
+	3 Data una stringa ASCII, stampare tutto ciò che compare prima della prima '/'
+
+
---
+++ 11_Esercitazione/01_contaA/main.go
@@ -0,0 +1,29 @@
+/* Leggere una stringa e stampare quante "a" contiene
+		. Caso ASCII
+		. Caso Generale
+*/
+
+package main
+
+import (
+		"fmt"
+		"bufio"
+		"os"
+)
+
+func main() {
+	fmt.Print("Inserisci una stringa: ")
+	b := bufio.NewScanner(os.Stdin)
+	b.Scan()
+
+	s := b.Text()
+	
+	var contaA int
+	for _, x := range s {
+		if x == 97 {
+			contaA++
+		}
+	}
+	fmt.Print("In \"", s, "\" ci sono: ", contaA, " a.\n")
+
+}
