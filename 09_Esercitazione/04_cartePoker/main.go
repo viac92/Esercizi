d027b726d0f60750b57c34de92505fa8932ba1c9
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-05 18:14:19 +0100

    risolto esercizio 4

---
+++ 09_Esercitazione/04_cartePoker/main.go
@@ -0,0 +1,46 @@
+// numerare carte da poker 13x4 = 52, numerarle da 0 a 51. Scrivere un programma che legge un numero da 0 a 51 e stampa la carta corrispondente. con simbolo ASCII
+
+package main
+
+import "fmt"
+
+// Seme dato un numero restituisce il seme 
+func Seme(n int) rune {
+	switch n / 13 {
+	case 0:
+		return '\u2665'
+	case 1:
+		return '\u2666'
+	case 2:
+		return '\u2663'
+	case 3:
+		return '\u2660'
+	default:
+		return '?'
+	}
+}
+
+// Carta dato un numero restituisce il valore della carta
+func Carta(n int) string {
+	switch n % 13 {
+	case 0: 
+		return "A"
+	case 9:
+		return "10"
+	case 10: 
+		return "J"
+	case 11:
+		return "Q"
+	case 12:
+		return "K"
+	default:
+		s := 49 + (n % 13)
+		return string(s) 
+	}
+}
+
+func main()	{
+	for i := 0; i < 52; i++ {
+	fmt.Println(Carta(i) + string(Seme(i)))
+	}
+}
