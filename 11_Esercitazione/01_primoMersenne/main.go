c5be421e6fe523f00d4d6c64f480acc5e109fa13
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-08 21:17:46 +0100

    corretto esercizio 1

--- 11_Esercitazione/01_primoMersenne/main.go
+++ 11_Esercitazione/01_primoMersenne/main.go
@@ -8,18 +8,51 @@ import (
 	"math"
 )
 
-func primiMersenne(n float64) (out int) {
-	out = int(math.Pow(2, n)) - 1
-	return 
+// dato un n restituisce l'n-esimo numero primo
+func numPrimi(n int) int {
+	var numero int = 2
+	var index int
+	var contaPrimi int	
+	//ciclo per numeri primi
+	for {
+		//ciclo per successione numeri
+		for {
+			index = 2
+			//ciclo per verifica numero primo
+			for index < numero {
+				if numero % index == 0 {
+					break
+				}	
+				index++
+			}
+			if index >= numero {
+				
+				break
+			}
+			numero++
+		} 
+
+		contaPrimi++
+		if contaPrimi == n {
+			break
+		}
+		numero++
+	}
+	return numero
 }
 
-func main()  {
+func numMersenne(n int) {
+	m := math.Pow(2, float64(numPrimi(n))) - 1
+	fmt.Println(m)
+
+}
+
+func main() {
 	var n int
 	fmt.Print("Inserisci un numero: ")
 	fmt.Scan(&n)
 
-	for i := 0; i < n; i++{
-		fmt.Println(primiMersenne(float64(i)))
+	for i := 0; i < n; i++ {
+	numMersenne(i + 1)
 	}
-	
 }
