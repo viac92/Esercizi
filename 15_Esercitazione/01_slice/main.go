d855cc3cad87eb95cd3f75fd8c8f096d82a921d7
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-18 10:47:49 +0100

    risotlo esercizio 1

---
+++ 15_Esercitazione/01_slice/main.go
@@ -0,0 +1,35 @@
+// Scrivere un programma con una funzione che decide se un numero è primo.
+// Nel main chiedere all'utente un valore n e inserire in una slice tutti i primi c = n.
+// Stampare la slice, la sua lunghezza e la sua capacità.
+
+package main
+
+import "fmt"
+
+func primo(n int) []int {
+	var numeriPrimi []int
+	numeriPrimi = make([]int, 0, n)
+	var flag bool = true 
+	//ciclo numeri da testare
+	for num := 2; num < n; num++ {
+		//ciclo controllo
+		for i := 2; i < num; i++ {
+			if num % i == 0 {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			numeriPrimi = append(numeriPrimi, num)
+		}
+		flag = true
+	}
+	return numeriPrimi
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Print(primo(n), cap(primo(n)), len(primo(n)), "\n")
+}
