db23e4472799aae7a45b56ad7b85f57e44733f4f
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-19 11:11:05 +0100

    continuato esercizio 2

--- 16_Esercitazioni/02_distGiorni/main.go
+++ 16_Esercitazioni/02_distGiorni/main.go
@@ -3,8 +3,47 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type data struct {
+	g, m, a int
+}
+
+func stringToData(s string) (data, error) {
+	var g, m, a int
+	var slash1 int
+	var err error
+	for i, char := range s {
+		if char == '/' && slash1 == 0 {
+			slash1 = i
+			g,err = strconv.Atoi(s[:i])
+		} else if char == '/' {
+			m,err = strconv.Atoi(s[slash1 + 1: i])
+			a,err = strconv.Atoi(s[i + 1 :])
+		}
+	}
+	return data{g, m, a}, err
+}
+
+func dataToString(d data) string {
+	return strconv.Itoa(d.g) + "/" + strconv.Itoa(d.m) + "/" + strconv.Itoa(d.a)
+}
+
+func distData(d1 data, d2 data) int {
+
+
+}
+
+
 
 func main() {
-	fmt.Print()
+	dataString := os.Args[1]
+	dataString2 := os.Args[2]
+
+	fmt.Println(stringToData(dataString))
+	fmt.Println(stringToData(dataString2))
 }
