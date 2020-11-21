b066b624eb9bd491ffb63c10fdbd02eda3709201
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-21 01:32:58 +0100

    risolto esercizio 2, da controllare

--- 16_Esercitazioni/02_distGiorni/main.go
+++ 16_Esercitazioni/02_distGiorni/main.go
@@ -6,43 +6,79 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"strconv"
 )
 
-type data struct {
+// Data contiene le date
+type Data struct {
 	g, m, a int
 }
 
-func stringToData(s string) (data, error) {
-	var g, m, a int
-	var slash1 int
-	var err error
-	for i, char := range s {
-		if char == '/' && slash1 == 0 {
-			slash1 = i
-			g,err = strconv.Atoi(s[:i])
-		} else if char == '/' {
-			m,err = strconv.Atoi(s[slash1 + 1: i])
-			a,err = strconv.Atoi(s[i + 1 :])
+func stringToData(str string) Data {
+	var dataOut Data
+
+	slash := strings.Index(str, "/")
+	slash2 := strings.LastIndex(str, "/")
+	
+	dataOut.g, _ = strconv.Atoi(str[:slash])
+	dataOut.m, _ = strconv.Atoi(str[slash+1:slash2])
+	dataOut.a, _ = strconv.Atoi(str[slash2+1:])
+
+	return dataOut 
+}
+
+func giorniMese(m int, a int) int {
+	switch m {
+	case 4, 6, 8, 9, 11:
+		return 30
+	case 2:
+		if a % 4 == 0 {
+			return 29
+		} 
+			return 28
+		
+	default: 
+		return 31
+	}
+}
+
+func distData(data1, data2 Data) int {
+	var giorniOut int
+	for  i := data1.a; i < data2.a; i++ {
+		if i % 4 == 0 {
+			giorniOut += 366
+		} else {
+			giorniOut += 365
 		}
 	}
-	return data{g, m, a}, err
-}
-
-func dataToString(d data) string {
-	return strconv.Itoa(d.g) + "/" + strconv.Itoa(d.m) + "/" + strconv.Itoa(d.a)
-}
-
-func distData(d1 data, d2 data) int {
 	
+	if data1.m > data2.m {
+		for i, a := data1.m, data1.a; i > data2.m; i-- {
+			giorniOut -= giorniMese(i, a)
+		}
+	} else {
+		for i, a := data1.m, data2.a; i < data2.m; i++ {
+			giorniOut += giorniMese(i, a)
+		}
+	}
+
+	giorniOut -= data1.g
+	giorniOut += data2.g
+
+	return giorniOut
 }
-
-
 
 func main() {
-	dataString := os.Args[1]
-	dataString2 := os.Args[2]
+	var str1, str2 string
+	
+	str1 = os.Args[1]
+	str2 = os.Args[2]
 
-	fmt.Println(stringToData(dataString))
-	fmt.Println(stringToData(dataString2))
+	data1 := stringToData(str1)
+	data2 := stringToData(str2)
+	fmt.Println(data1)
+	fmt.Println(data2)
+
+	fmt.Println(distData(data1, data2))
 }
