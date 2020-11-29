6f6957155bcdf0e069bd4d8ea34a4df3d97b38b1
viac92 <64146594+viac92@users.noreply.github.com>
2020-11-29 23:47:14 +0100

    aggiornato esercizio

--- 19_Esercitazione/01_provaServer/server.go
+++ 19_Esercitazione/01_provaServer/server.go
@@ -29,11 +29,44 @@ func quadratoHendler(w http.ResponseWriter, r *http.Request) {
 	}
 	png.Encode(w, i)
 }
+func kochCurve(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/koch.png" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+
+	i := image.NewRGBA(image.Rect(0, 0, 700, 700))
+	t := terrapin.NewTerrapin(i, terrapin.Position{200.0, 500.0})
+	for i := 0; i < 4; i++ {
+		kochCurveMaker(t, 4.0, 4)
+		t.Right(math.Pi - math.Pi / 2)
+	}
+	png.Encode(w, i)
+}
+
+func kochCurveMaker(t *terrapin.Terrapin, lung float64, liv int) {
+	if liv == 0 {
+		t.Forward(lung)
+		return
+	}
+	kochCurveMaker(t, lung, liv - 1)
+	t.Left(math.Pi / 3.0)
+	kochCurveMaker(t, lung, liv - 1)
+	t.Right(math.Pi - math.Pi / 3.0)
+	kochCurveMaker(t, lung, liv - 1)
+	t.Left(math.Pi / 3)
+	kochCurveMaker(t, lung, liv - 1)
+	
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/quadrato.png", quadratoHendler)
+	http.HandleFunc("/koch.png", kochCurve)
 
 	fmt.Print("Starting server port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
